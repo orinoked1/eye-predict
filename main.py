2adee0128fd25bed518effd2cd6a9ca9518438b9
Hadas <daseyd@gmail.com>
2019-03-12 17:56:39 +0200

    change folder name

--- main.py
+++ main.py
@@ -9,17 +9,19 @@ import matplotlib as mpl
 """
 Visualization
 """
+"""
 fixation_df = ds.get_fixation_df()
 scanpath_df = ds.get_scanpath_df()
 stimTypes = fixation_df.stimType.unique()
 vis.visualize(fixation_df, scanpath_df, stimTypes[0])
-
+"""
+num_epochs = 10
+batch_size = 64
 
 """Run model CNN simple"""
-use_cuda, log, x_dtype, y_dtype, n_epochs, criterion = run_model.init(10, "cnn_loss.txt")
-
-#chose the model to run
 model = models.cnnSimple()
+
+use_cuda, log, x_dtype, y_dtype, n_epochs, criterion = run_model.init(num_epochs, "cnn_loss.txt")
 
 print("preparing training and test sets")
 x_train, x_test, y_train, y_test = ds.get_train_test_dataset()
@@ -27,7 +29,7 @@ x_train, x_test, y_train, y_test = ds.get_train_test_dataset()
 X_train, X_val, Y_train, Y_val = ds.get_train_val_dataset(x_train, y_train)
 
 print("Start train")
-train_loss_curve, val_loss_curve = run_model.train(model, X_train, Y_train, X_val, Y_val, criterion, use_cuda, log, x_dtype, y_dtype, n_epochs)
+train_loss_curve, val_loss_curve = run_model.train_by_batches(model, X_train, Y_train, X_val, Y_val, criterion, use_cuda, log, x_dtype, y_dtype, n_epochs, batch_size)
 
 print("Run test")
 test_total_loss = run_model.test(model, x_test, y_test, criterion, log, x_dtype, y_dtype)
@@ -36,21 +38,6 @@ log.close()
 
 #torch.save(model.state_dict(), models.checkpoint_cnn_path())
 
-if use_cuda:
-    mpl.use('Agg')
-    import matplotlib.pyplot as plt
+plot = run_model.plot_train_val_loss_curve(use_cuda, n_epochs, train_loss_curve, val_loss_curve)
 
-    plt.ioff()  # http://matplotlib.org/faq/usage_faq.html (interactive mode)
-else:
-    import matplotlib.pyplot as plt
-
-plt.title('Loss function value for validation and train after each epoch')
-plt.xlabel('epoch')
-plt.ylabel('loss')
-
-epochs = list(range(1, n_epochs + 1))
-plt.plot(epochs, train_loss_curve, 'b', label='Q1 Train Data')
-plt.plot(epochs, val_loss_curve, 'r', label='Q1 Validation Data')
-
-plt.legend(loc='best')
-plt.savefig('cnn_train_val_plot.png')
+plot.show()
--- run_model.py
+++ run_model.py
@@ -4,6 +4,8 @@ import sys
 sys.path.append('../')
 from models import Variable
 from sklearn.utils import shuffle
+import matplotlib as mpl
+import numpy as np
 
 def init(epochs, log_file_name):
     use_cuda = torch.cuda.is_available()
@@ -16,7 +18,7 @@ def init(epochs, log_file_name):
     return  use_cuda, log, x_dtype, y_dtype, n_epochs, criterion
 
 
-def train(model, X_train, Y_train, X_val, Y_val, criterion, use_cuda, log, x_dtype, y_dtype, n_epochs):
+def train_by_one_epoch(model, X_train, Y_train, X_val, Y_val, criterion, use_cuda, log, x_dtype, y_dtype, n_epochs):
     if (use_cuda):
         model.cuda()
 
@@ -87,6 +89,90 @@ def train(model, X_train, Y_train, X_val, Y_val, criterion, use_cuda, log, x_dty
 
     return train_loss_curve, val_loss_curve
 
+def train_by_batches(model, X_train, Y_train, X_val, Y_val, criterion, use_cuda, log, x_dtype, y_dtype, n_epochs, batchSize):
+    if (use_cuda):
+        model.cuda()
+
+    print("use_cuda:",use_cuda)
+    print("num epochs:",n_epochs)
+    print("batch size: ", batchSize)
+
+    learning_rate = 1e-7
+
+    """
+    Train the network using Adam optimizer. 
+    Train the network and validate it for each line in the dataset 
+    """
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    n_train = batchSize
+    n_val = round(0.2 * batchSize)
+    train_loss_curve = []
+    val_loss_curve = []
+
+    for epoch in range(n_epochs):
+        for batch in range(round(len(X_train)/batchSize)):
+            """sample train val according to batch size"""
+            indices = shuffle(range(batchSize))
+            x = []
+            for i in indices:
+                x.append(X_train[i])
+            x = np.asanyarray(x)
+            y = np.take(Y_train, indices)
+
+            indx = shuffle(range(n_val))
+            x_val = []
+            for i in indx:
+                x_val.append(X_val[i])
+            x_val = np.asanyarray(x_val)
+            y_val = np.take(Y_val, indx)
+
+            x = torch.from_numpy(x).type(x_dtype)
+            y = torch.from_numpy(y).type(y_dtype)
+            x = x.view(n_train, 1, 431, 575)
+
+            x_val = torch.from_numpy(x_val).type(x_dtype)
+            y_val = torch.from_numpy(y_val).type(y_dtype)
+            x_val = x_val.view(n_val, 1, 431, 575)
+
+            model.train()
+            epoch_loss_mean = []
+            #Train the model
+            print("Train size:", n_train)
+
+            x_var = Variable(x)
+            y_var = Variable(y)
+
+            optimizer.zero_grad()
+            y_pred = model(x_var)
+
+            train_loss = criterion(y_pred, y_var)
+            train_loss.backward()
+            optimizer.step()
+            epoch_loss_mean.append(train_loss)
+
+            #compute train loss
+            train_total_loss = sum(epoch_loss_mean) / len(epoch_loss_mean)
+            train_loss_curve.append(train_total_loss.item())
+            print("Epoch: {0}, Train Loss: {1}, ".format(epoch, train_total_loss.item()))
+            log.write("Epoch: {0}, Train Loss: {1}, \n".format(epoch, train_total_loss.item()))
+
+            #compute Validation loss
+            model.eval()
+            epoch_loss_mean = []
+            x_var = Variable(x_val)
+            y_var = Variable(y_val)
+            y_pred = model(x_var)
+            test_loss = criterion(y_pred, y_var)
+            epoch_loss_mean.append(test_loss)
+            test_total_loss = sum(epoch_loss_mean) / len(epoch_loss_mean)
+            val_loss_curve.append(test_total_loss.item())
+            log.write("Epoch: {0}, Validation Loss: {1},\n ".format(epoch, test_total_loss.item()))
+            print("Epoch: {0}, Validation Loss: {1}, ".format(epoch, test_total_loss.item()))
+
+
+    return train_loss_curve, val_loss_curve
+
 def test(model, X_test, Y_test, criterion, log, x_dtype, y_dtype):
     # compute test loss
     model.eval()
@@ -107,3 +193,25 @@ def test(model, X_test, Y_test, criterion, log, x_dtype, y_dtype):
     print("Final Test Loss: {0}, ".format(test_total_loss.item()))
 
     return test_total_loss
+
+def plot_train_val_loss_curve(use_cuda, n_epochs, train_loss_curve, val_loss_curve):
+    if use_cuda:
+        mpl.use('Agg')
+        import matplotlib.pyplot as plt
+
+        plt.ioff()  # http://matplotlib.org/faq/usage_faq.html (interactive mode)
+    else:
+        import matplotlib.pyplot as plt
+
+    plt.title('Loss function value for validation and train after each epoch')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+
+    epochs = list(range(1, 850 + 1))
+    plt.plot(epochs, train_loss_curve, 'b', label='Q1 Train Data')
+    plt.plot(epochs, val_loss_curve, 'r', label='Q1 Validation Data')
+
+    plt.legend(loc='best')
+    plt.savefig('cnn_train_val_plot.png')
+
+    return plt
